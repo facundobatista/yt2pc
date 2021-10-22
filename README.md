3d817c3d2930b1bd24fe8d228fba7c1f4faabfca
Facundo Batista <facundo@taniquetil.com.ar>
2021-10-22 19:42:41 -0300

    First useful version.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # yt2pc
-YouTube to Podcast converter
+
+YouTube to Podcast converter. It gets audios from a YouTube playlist and prepares them to be consumed by a podcast client.
+
+**NOTE**: this is for personal usage only, get an audio and listen it yourself, do not distribute it.
+
+
+# How does this work?
+
+You need a server and setup `yt2pc` there. Then fill the config file for the playlists you want to convert, and run `yt2pc` like this:
+
+    fades yt2pc.py config_file.yaml
+
+You do NOT need to leave `yt2pc` running, is not a service; it will leave a bunch of files in a directory that needs to be served by a regular web server, though.
+
+The configuration file is simple. See this example:
+
+```yaml
+# base config
+main:
+  # the public URL from where the podcast is served
+  base-public-url: https://podcasts.yoursite.com/
+  # the directory where podcast files will be stored
+  podcast-dir: /var/lib/podcasts
+  # the history file
+  history-file: /var/lib/podcast_history.txt
+
+# collections of shows, each key is the "show id" and all data is specific for that show
+shows:
+  my-crazy-show:
+    # title and description for the podcast metadata
+    title: My Crazy Show
+    description: Stand up show about crazy people coding ad-hoc crazy software
+    # the URL of the YouTube playlist
+    url: https://www.youtube.com/playlist?list=238asd9asd...3.141592...83092183Dg
+    # when the playlist will be checked to find new episodes
+    cron: "00   10    *     *     6"  # m h (local) dom mon dow
+    # episodes before this date will be ignored
+    start-timestamp: 2021-09-18
+    # the URL of the podcast's image
+    image-url: https://crazy.software/wp-content/uploads/2019/02/pikachu.png
+```
+
+Ideally you should run `yt2pc` periodically, like every hour or so. Do not worry, it will NOT hit YouTube for new stuff everytime is run, but everytime is indicated in each podcast's `cron` (see config example above).
---
+++ yt2pc.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env fades
+
+import argparse
+import datetime
+import glob
+import json
+import logging
+import os
+import subprocess
+from dataclasses import dataclass
+
+import croniter  # fades
+import dateutil.parser  # fades python-dateutil
+import yaml  # fades
+import youtube_dl  # fades
+from feedgen.feed import FeedGenerator  # fades
+from dateutil.utils import default_tzinfo
+from dateutil.tz import tzoffset
+
+logger = logging.getLogger()
+h = logging.StreamHandler()
+h.setFormatter(logging.Formatter("%(asctime)s %(levelname)-10s %(message)s"))
+logger.addHandler(h)
+logger.setLevel(logging.INFO)
+
+DFLT_TZ = tzoffset("UTC", 0)
+
+
+@dataclass
+class PlayListItem:
+    description: str
+    item_id: str
+    webpage_url: str
+    title: str
+    date: datetime.date
+    best_format: str
+
+    def __str__(self):
+        return f"<PlayListItem id={self.item_id} date={self.date:%Y-%m-%d}>"
+
+
+def get_playlist_content(playlist_url):
+    """Get the content of a YouTube playlist."""
+    # youtbe-dl is in the path because `youtube_dl` is in the virtualenv
+    cmd = ["youtube-dl", "--skip-download", "--print-json", playlist_url]
+    logger.info("Getting playlist metadata")
+    proc = subprocess.run(cmd, check=True, capture_output=True, text=True)
+
+    results = []
+    for line in proc.stdout.split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+        data = json.loads(line)
+
+        date = default_tzinfo(dateutil.parser.parse(data['upload_date']), DFLT_TZ)
+        for fmt in data['formats']:
+            if fmt['ext'] == "m4a":
+                best_format = fmt['format_id']
+                break
+        else:
+            raise ValueError(f"Best format not found in {data['formats']}")
+
+        plitem = PlayListItem(
+            description=data['description'],
+            item_id=data['display_id'],
+            title=data['fulltitle'],
+            webpage_url=data['webpage_url'],
+            date=date,
+            best_format=best_format,
+        )
+        results.append(plitem)
+    return results
+
+
+def report_progress(info):
+    """Report download."""
+    total = info['total_bytes']
+    dloaded = info['downloaded_bytes']
+    size_mb = total // 1024 ** 2
+    perc = dloaded * 100.0 / total
+    print(f"{perc:.1f}% of {size_mb:d} MB\r", end='', flush=True)
+
+
+def _download_and_process(base_path, url, video_format):
+    """Download from YouTube, showing process, and leave a .mp3."""
+    logger.info("Download episode %s", base_path)
+
+    # get from yt
+    conf = {
+        'outtmpl': base_path,
+        'progress_hooks': [report_progress],
+        'quiet': True,
+        'format': video_format,
+    }
+    with youtube_dl.YoutubeDL(conf) as ydl:
+        ydl.download([url])
+
+    # convert to mp3
+    logger.info("    converting to mp3")
+    mp3_name = base_path + ".mp3"
+    cmd = [
+        "ffmpeg", "-loglevel", "error",
+        "-i", base_path,
+        "-vn", "-ab", "128k", "-ar", "44100",
+        mp3_name,
+    ]
+
+    subprocess.run(cmd, check=True)
+    os.unlink(base_path)
+    logger.info("    done")
+
+
+def download(show_config, main_config):
+    """Download a show."""
+    playlist = get_playlist_content(show_config['url'])
+
+    show_id = show_config['id']
+    mp3_location = main_config['podcast-dir']
+    already_downloaded = glob.glob(os.path.join(mp3_location, show_id) + "*")
+    already_downloaded = [os.path.basename(x) for x in already_downloaded]
+
+    # build the filename with the show id and the show hours for the ones we need to download
+    for item in playlist:
+        if show_config['start-timestamp'] > item.date:
+            logger.info("Ignoring episode (before start timestamp): %s", item)
+            continue
+
+        base_name = f"{show_id}-{item.date:%Y%m%d}-{item.item_id}"
+        if any(x.startswith(base_name) for x in already_downloaded):
+            logger.info("Ignoring episode (already downloaded): %s", item)
+            continue
+
+        logger.info("Downloading episode: %s", item)
+        base_path = os.path.join(mp3_location, base_name)
+        _download_and_process(base_path, item.webpage_url, item.best_format)
+
+    # prepare some metadata from the playlist to write in the podcast
+    metadata = {item.item_id: item for item in playlist}
+
+    write_podcast(show_config, main_config, metadata)
+
+
+def check_show(show_config, last_process, main_config):
+    """Check for a specific show."""
+    download(show_config, main_config)
+    now = datetime.datetime.now()
+
+    if last_process is None:
+        # never did it before, do it now
+        logger.info("    downloading show for the first time")
+        download(show_config, main_config)
+    else:
+        from_cron = croniter.croniter(show_config['cron'], last_process)
+        next_date = from_cron.get_next(datetime.datetime)
+        logger.info("Next date to check: %s", next_date)
+        if next_date > now:
+            logger.info("    still in the future, pass")
+        else:
+            logger.info("    downloading show again")
+            download(show_config, main_config)
+    return now
+
+
+def write_podcast(show_config, main_config, all_metadata):
+    """Create the podcast file for a specific show (for all episodes)."""
+    fg = FeedGenerator()
+    fg.load_extension('podcast')
+
+    base_public_url = main_config["base-public-url"]
+    show_id = show_config["id"]
+    url = "{}{}.xml".format(base_public_url, show_id)
+    fg.id(show_id)
+    fg.title(show_config["title"])
+    fg.description(show_config["description"])
+    image_url = show_config.get("image-url")
+    if image_url is not None:
+        fg.image(image_url)
+    fg.link(href=url, rel='self')
+
+    # collect all mp3s for the given show
+    all_mp3s = glob.glob(os.path.join(main_config["podcast-dir"], f"{show_id}-*.mp3"))
+
+    for filepath in all_mp3s:
+        filename = os.path.basename(filepath)
+        mp3_id = filename.split('.')[0]
+        episode_id = mp3_id.split("-")[2]
+        ep_metadata = all_metadata.get(episode_id)
+        if ep_metadata is None:
+            logger.warning("Warning: metadata not found for episode %s", episode_id)
+            continue
+
+        mp3_size = os.stat(filepath).st_size
+        mp3_url = base_public_url + filename
+
+        # build the rss entry
+        fe = fg.add_entry()
+        fe.id(mp3_id)
+        fe.pubdate(ep_metadata.date)
+        fe.title(ep_metadata.title)
+        fe.description(ep_metadata.description)
+        fe.enclosure(mp3_url, str(mp3_size), 'audio/mpeg')
+
+    fg.rss_str(pretty=True)
+    fg.rss_file(os.path.join(main_config["podcast-dir"], f'{show_id}.xml'))
+
+
+class History:
+    """Manage the history file."""
+    def __init__(self, history_file):
+        self.history_file = history_file
+
+        # (try to) open it
+        if os.path.exists(history_file):
+            with open(history_file, 'rt', encoding='utf8') as fh:
+                self.data = data = {}
+                for line in fh:
+                    show_id, last_timestamp = line.strip().split()
+                    data[show_id] = dateutil.parser.parse(last_timestamp)
+        else:
+            self.data = {}
+
+    def get(self, show_id):
+        """Get the last process for given show_id (if any)."""
+        return self.data.get(show_id)
+
+    def _save(self):
+        """Save the content to disk."""
+        temp_path = self.history_file + ".temp"
+        with open(temp_path, 'wt', encoding='utf8') as fh:
+            for show_id, last_time in sorted(self.data.items()):
+                fh.write("{} {}\n".format(show_id, last_time.isoformat()))
+
+        os.rename(temp_path, self.history_file)
+
+    def set(self, show_id, last_run):
+        """Set the last process for the given show_id to 'now' and save."""
+        self.data[show_id] = last_run
+        self._save()
+
+
+def load_config(config_file_path, selected_show):
+    """Load the configuration file and validate format."""
+    with open(config_file_path, 'rt', encoding='utf8') as fh:
+        raw_config = yaml.safe_load(fh)
+
+    if not isinstance(raw_config, dict):
+        raise ValueError("Bad general config format, must be a dict/map.")
+
+    # main section
+    main_keys = {'base-public-url', 'podcast-dir', 'history-file'}
+    main = raw_config.get('main', ())
+    missing = main_keys - set(main)
+    if missing:
+        raise ValueError(f"Missing keys in main config: {missing}")
+
+    # shows section
+    shows = []
+    show_keys = {'title', 'description', 'url', 'cron', 'start-timestamp'}
+    for show_id, show_data in raw_config['shows'].items():
+        if not show_id.isalnum():
+            raise ValueError(
+                "Bad format for show id {!r} (must be alphanumerical)".format(show_id))
+
+        if selected_show is not None and selected_show != show_id:
+            logger.warning("Ignoring config because not selected show: %r", show_id)
+            continue
+
+        missing = show_keys - set(show_data)
+        if missing:
+            raise ValueError("Missing keys {} for show id {}".format(missing, show_id))
+
+        # ensure we always have a timezoned datetime
+        tstamp = show_data['start-timestamp']
+        tstamp = datetime.datetime.fromordinal(tstamp.toordinal())
+        show_data['start-timestamp'] = default_tzinfo(tstamp, DFLT_TZ)
+
+        show_data['id'] = show_id
+        shows.append(show_data)
+
+    return dict(main=main, shows=shows)
+
+
+def main(config_file_path, selected_show=None):
+    """Main entry point."""
+    # open the config file
+    try:
+        config = load_config(config_file_path, selected_show)
+    except ValueError as exc:
+        logger.error("Problem loading config: %s", exc)
+        exit()
+
+    logger.info("Loaded config for shows %s", sorted(x['id'] for x in config['shows']))
+
+    # open the history file
+    history = History(config['main']['history-file'])
+
+    for show_data in config['shows']:
+        show_id = show_data['id']
+        logger.info("Processing show %r", show_id)
+        last_process = history.get(show_id)
+        logger.info("    last process: %s", last_process)
+        last_run = check_show(show_data, last_process, config['main'])
+        history.set(show_id, last_run)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--show', help="Work with this show only.")
+    parser.add_argument('--quiet', action='store_true', help="Be quiet, unless any issue is found")
+    parser.add_argument('config_file', help="The configuration file")
+    args = parser.parse_args()
+
+    # parse input
+    if args.quiet:
+        logger.setLevel(logging.WARNING)
+
+    main(args.config_file, args.show)
